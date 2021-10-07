425af2716dd4b09eb8c297752eb6badaa28b3a9a
Ezezsg <ezequielfperez@gmail.com>
2021-10-07 17:38:23 -0300

    primer2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyAA2yip6ll3GuLvZ0U6JfcRhDq22BP0I7s",
+    authDomain: "canactivate-976aa.firebaseapp.com",
+    projectId: "canactivate-976aa",
+    storageBucket: "canactivate-976aa.appspot.com",
+    messagingSenderId: "523338738948",
+    appId: "1:523338738948:web:e2e721cd5ade3b7f857451"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyAA2yip6ll3GuLvZ0U6JfcRhDq22BP0I7s",
+    authDomain: "canactivate-976aa.firebaseapp.com",
+    projectId: "canactivate-976aa",
+    storageBucket: "canactivate-976aa.appspot.com",
+    messagingSenderId: "523338738948",
+    appId: "1:523338738948:web:e2e721cd5ade3b7f857451"
+  }
+  
 };
 
 /*
